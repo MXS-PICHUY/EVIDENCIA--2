8c19d9c869e446efecf91eff4238fae0cdb43add
MXS-PICHUY <84110320+MXS-PICHUY@users.noreply.github.com>
2021-05-12 20:42:04 -0500

    Delete IMPORT RE #1.py

--- IMPORT RE #1.py
+++
@@ -1,9 +0,0 @@
-import re 
-frase = "Cython no es ningún lenguaje de programación y Nython tampoco pero Python sí" 
-patron = '^.ython' 
-palabras = re.findall(patron, frase) 
-print(palabras) 
-patron = '\\s.ython' 
-palabras = re.findall(patron, frase) 
-print(palabras) 
- 
